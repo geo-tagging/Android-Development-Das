ee5917d45d3e19b3851b416e5495a3e8c6931901
Gina Rachima Kesoema Putri <ginarachima@gmail.com>
2024-10-15 22:52:56 +0800

    #2 Refactor : Add new data item into DownloadResponse and OptionResponse, adjust it based on api. Make function to parse data to Entity in OptionResponse.

--- app/src/main/java/com/dicoding/geotaggingjbg/data/response/DownloadResponse.kt
+++ app/src/main/java/com/dicoding/geotaggingjbg/data/response/DownloadResponse.kt
@@ -16,11 +16,17 @@ data class DataItem(
 	@field:SerializedName("id_lokasi")
 	val idLokasi: Int,
 
+	@field:SerializedName("petak_ukur")
+	val petakUkur: String,
+
 	@field:SerializedName("latitude")
 	val latitude: String,
 
 	@field:SerializedName("id_sk")
 	val idSk: Int,
+
+	@field:SerializedName("id_skKerja")
+	val idSkKerja: Int,
 
 	@field:SerializedName("northing")
 	val northing: String,
@@ -34,11 +40,17 @@ data class DataItem(
 	@field:SerializedName("id_status")
 	val idStatus: Int,
 
+	@field:SerializedName("id_statusAreaTanam")
+	val idStatusAreaTanam: Int,
+
 	@field:SerializedName("tanggal_tanam")
 	val tanggalTanam: String,
 
 	@field:SerializedName("diameter")
-	val diameter: Double,
+	val diameter: Int,
+
+	@field:SerializedName("status_areaTanam")
+	val statusAreaTanam: String,
 
 	@field:SerializedName("action")
 	val action: String,
@@ -55,8 +67,14 @@ data class DataItem(
 	@field:SerializedName("easting")
 	val easting: String,
 
+	@field:SerializedName("id_petak")
+	val idPetak: Int,
+
 	@field:SerializedName("id_tanaman")
 	val idTanaman: Int,
+
+	@field:SerializedName("kategori")
+	val kategori: String,
 
 	@field:SerializedName("nama")
 	val nama: String,
@@ -76,11 +94,14 @@ data class DataItem(
 	@field:SerializedName("elevasi")
 	val elevasi: String,
 
+	@field:SerializedName("sk_kerja")
+	val skKerja: String,
+
 	@field:SerializedName("id_action")
 	val idAction: Int,
 
 	@field:SerializedName("tinggi")
-	val tinggi: Double,
+	val tinggi: Int,
 
 	@field:SerializedName("status")
 	val status: String,
--- app/src/main/java/com/dicoding/geotaggingjbg/data/response/OptionResponse.kt
+++ app/src/main/java/com/dicoding/geotaggingjbg/data/response/OptionResponse.kt
@@ -3,11 +3,17 @@ package com.dicoding.geotaggingjbg.data.response
 import com.dicoding.geotaggingjbg.data.database.JenisEntity
 import com.dicoding.geotaggingjbg.data.database.KegiatanEntity
 import com.dicoding.geotaggingjbg.data.database.LokasiEntity
+import com.dicoding.geotaggingjbg.data.database.PetakEntity
 import com.dicoding.geotaggingjbg.data.database.SkEntity
+import com.dicoding.geotaggingjbg.data.database.SkKerjaEntity
+import com.dicoding.geotaggingjbg.data.database.StatusAreaTanamEntity
 import com.dicoding.geotaggingjbg.data.database.StatusEntity
 import com.google.gson.annotations.SerializedName
 
 data class OptionResponse(
+
+	@field:SerializedName("tb_majorArea")
+	val tbMajorArea: List<TbMajorAreaItem>,
 
 	@field:SerializedName("tb_jenis")
 	val tbJenis: List<TbJenisItem>,
@@ -18,11 +24,20 @@ data class OptionResponse(
 	@field:SerializedName("tb_kegiatan")
 	val tbKegiatan: List<TbKegiatanItem>,
 
+	@field:SerializedName("tb_petakUkur")
+	val tbPetakUkur: List<TbPetakUkurItem>,
+
 	@field:SerializedName("tb_action")
 	val tbAction: List<TbActionItem>,
 
+	@field:SerializedName("tb_statusAreaTanam")
+	val tbStatusAreaTanam: List<TbStatusAreaTanamItem>,
+
 	@field:SerializedName("tb_sk")
 	val tbSk: List<TbSkItem>,
+
+	@field:SerializedName("tb_skKerja")
+	val tbSkKerja: List<TbSkKerjaItem>,
 
 	@field:SerializedName("message")
 	val message: String,
@@ -31,25 +46,25 @@ data class OptionResponse(
 	val tbLokasi: List<TbLokasiItem>
 )
 
-data class TbLokasiItem(
+data class TbPetakUkurItem(
 
 	@field:SerializedName("createdAt")
-	val createdAt: Any,
+	val createdAt: String,
 
-	@field:SerializedName("id_lokasi")
-	val idLokasi: Int,
+	@field:SerializedName("id_petak")
+	val idPetak: Int,
 
-	@field:SerializedName("lokasi")
-	val lokasi: String,
+	@field:SerializedName("petak_ukur")
+	val petakUkur: String,
 
 	@field:SerializedName("updatedAt")
-	val updatedAt: Any
+	val updatedAt: String
 )
 
 data class TbJenisItem(
 
 	@field:SerializedName("createdAt")
-	val createdAt: Any,
+	val createdAt: String,
 
 	@field:SerializedName("nama")
 	val nama: String,
@@ -61,22 +76,31 @@ data class TbJenisItem(
 	val kategori: String,
 
 	@field:SerializedName("updatedAt")
-	val updatedAt: Any
+	val updatedAt: String
 )
 
-data class TbKegiatanItem(
+data class TbLokasiItem(
+
+	@field:SerializedName("id_major")
+	val idMajor: Int,
 
 	@field:SerializedName("createdAt")
-	val createdAt: Any,
+	val createdAt: String,
 
-	@field:SerializedName("kegiatan")
-	val kegiatan: String,
+	@field:SerializedName("id_lokasi")
+	val idLokasi: Int,
 
-	@field:SerializedName("id_kegiatan")
-	val idKegiatan: Int,
+	@field:SerializedName("lokasi")
+	val lokasi: String,
+
+	@field:SerializedName("kecamatan")
+	val kecamatan: String,
+
+	@field:SerializedName("region")
+	val region: String,
 
 	@field:SerializedName("updatedAt")
-	val updatedAt: Any
+	val updatedAt: String
 )
 
 data class TbSkItem(
@@ -85,19 +109,49 @@ data class TbSkItem(
 	val skppkh: String,
 
 	@field:SerializedName("createdAt")
-	val createdAt: Any,
+	val createdAt: String,
 
 	@field:SerializedName("id_sk")
 	val idSk: Int,
 
 	@field:SerializedName("updatedAt")
-	val updatedAt: Any
+	val updatedAt: String
+)
+
+data class TbSkKerjaItem(
+
+	@field:SerializedName("createdAt")
+	val createdAt: String,
+
+	@field:SerializedName("sk_kerja")
+	val skKerja: String,
+
+	@field:SerializedName("id_skKerja")
+	val idSkKerja: Int,
+
+	@field:SerializedName("updatedAt")
+	val updatedAt: String
+)
+
+data class TbKegiatanItem(
+
+	@field:SerializedName("createdAt")
+	val createdAt: String,
+
+	@field:SerializedName("kegiatan")
+	val kegiatan: String,
+
+	@field:SerializedName("id_kegiatan")
+	val idKegiatan: Int,
+
+	@field:SerializedName("updatedAt")
+	val updatedAt: String
 )
 
 data class TbStatusItem(
 
 	@field:SerializedName("createdAt")
-	val createdAt: Any,
+	val createdAt: String,
 
 	@field:SerializedName("id_status")
 	val idStatus: Int,
@@ -106,13 +160,31 @@ data class TbStatusItem(
 	val status: String,
 
 	@field:SerializedName("updatedAt")
-	val updatedAt: Any
+	val updatedAt: String
+)
+
+data class TbMajorAreaItem(
+
+	@field:SerializedName("id_major")
+	val idMajor: Int,
+
+	@field:SerializedName("createdAt")
+	val createdAt: String,
+
+	@field:SerializedName("counter")
+	val counter: Int,
+
+	@field:SerializedName("instansi")
+	val instansi: String,
+
+	@field:SerializedName("updatedAt")
+	val updatedAt: String
 )
 
 data class TbActionItem(
 
 	@field:SerializedName("createdAt")
-	val createdAt: Any,
+	val createdAt: String,
 
 	@field:SerializedName("action")
 	val action: String,
@@ -121,8 +193,24 @@ data class TbActionItem(
 	val idAction: Int,
 
 	@field:SerializedName("updatedAt")
-	val updatedAt: Any
+	val updatedAt: String
 )
+
+data class TbStatusAreaTanamItem(
+
+	@field:SerializedName("createdAt")
+	val createdAt: String,
+
+	@field:SerializedName("id_statusAreaTanam")
+	val idStatusAreaTanam: Int,
+
+	@field:SerializedName("status_areaTanam")
+	val statusAreaTanam: String,
+
+	@field:SerializedName("updatedAt")
+	val updatedAt: String
+)
+
 
 fun TbJenisItem.toEntity(): JenisEntity {
 	return JenisEntity(
@@ -152,5 +240,23 @@ fun TbSkItem.toEntity(): SkEntity {
 	return SkEntity(
 		id = this.idSk,
 		sk = this.skppkh
+	)
+}
+fun TbPetakUkurItem.toEntity() : PetakEntity {
+	return PetakEntity(
+		id = this.idPetak,
+		petakUkur = this.petakUkur
+	)
+}
+fun TbSkKerjaItem.toEntity() : SkKerjaEntity {
+	return SkKerjaEntity(
+		id = this.idSkKerja,
+		skKerja = this.skKerja
+	)
+}
+fun TbStatusAreaTanamItem.toEntity() : StatusAreaTanamEntity {
+	return StatusAreaTanamEntity(
+		id = this.idStatusAreaTanam,
+		statusAreaTanam = this.statusAreaTanam
 	)
 }
